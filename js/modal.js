a5d4fb7bae08fb0be65a12191ce4eb23291ce32a
Ilia_Khrebtov <Ilia_Khrebtov@epam.com>
2021-08-19 21:12:04 +0300

    add keyboard a11y to cards

--- js/modal.js
+++ js/modal.js
@@ -4,8 +4,34 @@ const modalImageTwo = document.getElementById("modal-image2");
 
 const modalCardButton = document.getElementById("modal-card-button");
 const modalImageOneButton = document.getElementById("modal-image1-button");
+const modalImageOneClickable = document.getElementById(
+  "modal-image1-clickable"
+);
 const modalImageTwoButton = document.getElementById("modal-image2-button");
+const modalImageTwoClickable = document.getElementById(
+  "modal-image2-clickable"
+);
 
-modalCardButton.addEventListener("click", () => modalCard.querySelector("img").focus());
-modalImageOneButton.addEventListener("click", () => modalImageOne.querySelector("img").focus());
-modalImageTwoButton.addEventListener("click", () => modalImageTwo.querySelector("img").focus());
+function openModal(modal) {
+  modal.classList.add("is-active");
+  modal.querySelector("img").focus();
+}
+
+function handleSpace(modal) {
+  return ({ code }) => {
+    if (code === "Space") openModal(modal);
+  };
+}
+
+modalCardButton.addEventListener("click", () => openModal(modalCard));
+modalImageOneButton.addEventListener("click", () => openModal(modalImageOne));
+modalImageOneClickable.addEventListener("click", () =>
+  openModal(modalImageOne)
+);
+modalImageTwoButton.addEventListener("click", () => openModal(modalImageTwo));
+modalImageTwoClickable.addEventListener("click", () =>
+  openModal(modalImageTwo)
+);
+
+modalImageOneClickable.addEventListener('keydown', handleSpace(modalImageOne));
+modalImageTwoClickable.addEventListener('keydown', handleSpace(modalImageTwo));
