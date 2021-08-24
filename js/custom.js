965a3408fe6a07d21f0e853852201491a61e858f
Veranika Shneidzer <veranika.shneidzer@burberry.com>
2021-08-24 22:09:48 +0300

    [TASK-5] Feature: Implement accessibility for tab navigation

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,12 @@
+const constants = {
+  KEY_CODE_ENTER: 13,
+  KEY_CODE_SPACE: 32,
+  KEY_CODE_END: 35,
+  KEY_CODE_HOME: 36,
+  KEY_CODE_ARROW_LEFT: 37,
+  KEY_CODE_ARROW_RIGHT: 39,
+};
+
 (function() {
   const burger = document.querySelector(".burger");
   const menu = document.querySelector("#" + burger.dataset.target);
@@ -28,29 +37,88 @@
   skipButtonMenu.addEventListener("click", function() {
     skipLink.classList.remove("visible");
     skipButtonMenu.classList.remove("visible");
-  })
+  });
+
+  const navEls = document.querySelectorAll(".tab-button");
+  navEls.forEach(function(navEl) {
+    if (navEl.classList.contains("is-active")) {
+      navEl.ariaSelected = true;
+    }
+  });
 })();
 
-document.querySelectorAll("#nav li").forEach(function(navEl) {
+document.addEventListener('keydown', (event) => {
+  const { keyCode } = event;
+
+  if (keyCode === constants.KEY_CODE_END || keyCode === constants.KEY_CODE_HOME) {
+    event.preventDefault();
+  }
+});
+
+document.querySelectorAll(".tab-button").forEach(function(navEl) {
   navEl.onclick = function() {
     toggleTab(this.id, this.dataset.target);
   };
+
+  navEl.onkeydown = function(event) {
+    const { keyCode } = event;
+    const currentId = +this.id.slice(-1) - 1;
+
+    if (keyCode === constants.KEY_CODE_ARROW_LEFT) {
+      onfocusTab(this.id, currentId, true);
+    }
+
+    if (keyCode === constants.KEY_CODE_ARROW_RIGHT) {
+      onfocusTab(this.id, currentId, false);
+    }
+
+    if (keyCode === constants.KEY_CODE_END) {
+      onfocusEdge(true);
+    }
+
+    if (keyCode === constants.KEY_CODE_HOME) {
+      onfocusEdge(false);
+    }
+
+    if (keyCode === constants.KEY_CODE_ENTER || keyCode === constants.KEY_CODE_SPACE) {
+      toggleTab(this.id, this.dataset.target);
+    }
+  };
 });
 
+function onfocusEdge(isFocusEndElement) {
+  const navEls = document.querySelectorAll(".tab-button");
+
+  navEls[isFocusEndElement ? navEls.length - 1 : 0].focus();
+};
+
+function onfocusTab(selectedNav, id, isLeftArrow) {
+  const navEls = document.querySelectorAll(".tab-button");
+
+  if (isLeftArrow && id !== 0) {
+    navEls[id - 1].focus();
+  }
+  if (!isLeftArrow && id !== navEls.length - 1) {
+    navEls[id + 1].focus();
+  }
+};
+
 function toggleTab(selectedNav, targetId) {
-  const navEls = document.querySelectorAll("#nav li");
+  const navEls = document.querySelectorAll(".tab-button");
 
   navEls.forEach(function(navEl) {
     if (navEl.id == selectedNav) {
       navEl.classList.add("is-active");
+      navEl.ariaSelected = true;
     } else {
       if (navEl.classList.contains("is-active")) {
         navEl.classList.remove("is-active");
+        navEl.ariaSelected = false;
       }
     }
   });
 
-  const tabs = document.querySelectorAll(".tab-pane");
+  const tabs = document.querySelectorAll(".tab-panel");
 
   tabs.forEach(function(tab) {
     if (tab.id == targetId) {
