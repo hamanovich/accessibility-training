d8f87e211d0c945b22dc219fa156eda890e5d2c6
Uladzimir Atroshchanka <uladzimir_atroshchanka@epam.com>
2021-10-19 17:28:24 +0300

    feat: hw #4 done + fixes from prev hws

--- js/custom.js
+++ js/custom.js
@@ -7,35 +7,173 @@
   });
 })();
 
-document.querySelectorAll("#nav li").forEach(function(navEl) {
-  navEl.onclick = function() {
-    toggleTab(this.id, this.dataset.target);
+(function () {
+  var tablist = document.querySelector('[role="tablist"]');
+  var tabs;
+  var panels;
+
+  generateArrays();
+
+  function generateArrays () {
+    tabs = document.querySelectorAll('[role="tab"]');
+    panels = document.querySelectorAll('[role="tabpanel"]');
   };
-});
 
-function toggleTab(selectedNav, targetId) {
-  var navEls = document.querySelectorAll("#nav li");
+  var keys = {
+    end: 35,
+    home: 36,
+    left: 37,
+    up: 38,
+    right: 39,
+    down: 40,
+    delete: 46,
+    enter: 13,
+    space: 32
+  };
 
-  navEls.forEach(function(navEl) {
-    if (navEl.id == selectedNav) {
-      navEl.classList.add("is-active");
+  var direction = {
+    37: -1,
+    38: -1,
+    39: 1,
+    40: 1
+  };
+
+  for (i = 0; i < tabs.length; ++i) {
+    addListeners(i);
+  };
+
+  function addListeners (index) {
+    tabs[index].addEventListener('click', clickEventListener);
+    tabs[index].addEventListener('keydown', keydownEventListener);
+    tabs[index].addEventListener('keyup', keyupEventListener);
+
+    tabs[index].index = index;
+  };
+
+  function clickEventListener (event) {
+    if (event.target.classList.contains('tab')) {
+      activateTab(event.target, false);
+    }
+  };
+
+  function keydownEventListener (event) {
+    var key = event.keyCode;
+    switch (key) {
+      case keys.end:
+        event.preventDefault();
+        focusLastTab();
+        break;
+      case keys.home:
+        event.preventDefault();
+        focusFirstTab();
+        break;
+
+      case keys.left: 
+      case keys.right: 
+        event.preventDefault();
+        break;
+      case keys.up:
+      case keys.down:
+        determineOrientation(event);
+        break;
+    };
+  };
+
+  function keyupEventListener (event) {
+    console.log(event);
+    var key = event.keyCode;
+
+    switch (key) {
+      case keys.left:
+      case keys.right:
+        determineOrientation(event);
+        break;
+      case keys.enter:
+      case keys.space:
+        activateTab(event.target);
+        break;
+    };
+  };
+
+  function determineOrientation (event) {
+    console.log(event);
+    var key = event.keyCode;
+    var vertical = tablist.getAttribute('aria-orientation') == 'vertical';
+    var proceed = false;
+
+    if (vertical) {
+      if (key === keys.up || key === keys.down) {
+        event.preventDefault();
+        proceed = true;
+      };
     } else {
-      if (navEl.classList.contains("is-active")) {
-        navEl.classList.remove("is-active");
+      if (key === keys.left || key === keys.right) {
+        proceed = true;
+      };
+    };
+
+    if (proceed) {
+      switchTabOnArrowPress(event);
+    };
+  };
+
+  function switchTabOnArrowPress (event) {
+    var pressed = event.keyCode;
+
+    if (direction[pressed]) {
+      var target = event.target;
+      console.log(target);
+      if (target.index !== undefined) {
+        if (tabs[target.index + direction[pressed]]) {
+          tabs[target.index + direction[pressed]].focus();
+        }
+        else if (pressed === keys.left || pressed === keys.up) {
+          focusLastTab();
+        }
+        else if (pressed === keys.right || pressed == keys.down) {
+          focusFirstTab();
+        };
+      };
+    };
+  };
+
+  function activateTab (tab, setFocus) {
+    setFocus = setFocus || true;
+    deactivateTabs();
+    tab.removeAttribute('tabindex');
+
+    tab.setAttribute('aria-selected', 'true');
+    tab.classList.add('is-active');
+
+    var controls = tab.getAttribute('aria-controls');
+    document.getElementById(controls).removeAttribute('hidden');
+    if (setFocus) {
+      tab.focus();
+    };
+  };
+
+  function deactivateTabs () {
+    for (t = 0; t < tabs.length; t++) {
+      tabs[t].setAttribute('tabindex', '-1');
+      tabs[t].setAttribute('aria-selected', 'false');
+      if (tabs[t].classList.contains("is-active")) {
+        tabs[t].classList.remove("is-active");
       }
-    }
-  });
+    };
 
-  var tabs = document.querySelectorAll(".tab-pane");
+    for (p = 0; p < panels.length; p++) {
+      panels[p].setAttribute('hidden', 'hidden');
+    };
+  };
 
-  tabs.forEach(function(tab) {
-    if (tab.id == targetId) {
-      tab.style.display = "block";
-    } else {
-      tab.style.display = "none";
-    }
-  });
-}
+  function focusFirstTab () {
+    tabs[0].focus();
+  };
+
+  function focusLastTab () {
+    tabs[tabs.length - 1].focus();
+  };
+}());
 
 const inputEl = document.getElementById('enterSomethingInput');
 
@@ -58,8 +196,7 @@ document.addEventListener('click', () => {
       clearInterval(interval);
     }
   }, 1000)
-  
-})
+}, { once: true })
 
 const navBar = document.getElementById('navbarMenu');
 const skipNavBtn = document.querySelector('.skip-nav-link');
@@ -81,16 +218,66 @@ const setIsModalOpen = () => {
 }
 
 document.addEventListener('focusin', (event) => {
-  console.log(event)
-  if (event.target.className !== "skip-nav-item") {
+  if (event.target.className !== "skip-nav-item" && !skipNavList.classList.contains('display-none')) {
     skipNavList.classList.add('display-none');
-    setIsModalOpen();
   }
 }, true);
 
-document.addEventListener("keydown", function(event) {
-  if (isModalOpen && event.keyCode == 27) {
+document.addEventListener('focusout', (event) => {
+  if (event.relatedTarget === null) {
     skipNavList.classList.add('display-none');
-    setIsModalOpen();
+  } 
+}, true);
+
+const tabItems = document.querySelectorAll('.tab');
+tabItems.forEach((tab) => tab.addEventListener('click', (e) => e.preventDefault()));
+
+const administrationBtn = document.getElementById('administration-button');
+const administrationList = document.getElementById('administration-list');
+const administrationListItems = administrationList.childNodes;
+
+const hideAdminMenu = () => {
+  administrationList.setAttribute('hidden', 'hidden');
+  administrationList.setAttribute('aria-expanded', 'false');
+}
+
+const showAdminMenu = () => {
+  administrationList.removeAttribute('hidden');
+  administrationList.setAttribute('aria-expanded', 'true');
+}
+
+administrationBtn.addEventListener('click', () => {
+  if (administrationList.hasAttribute('hidden')) {
+    showAdminMenu();
+  } else {
+    hideAdminMenu();
+  }
+})
+
+administrationList.addEventListener('mouseout', (event) => {
+  if (event.target.parentNode.className !== 'administration-list__item') {
+    setTimeout(hideAdminMenu, 2000);
   }
 }, true);
+
+administrationListItems.forEach(el => el.addEventListener('focusout', (event) => {
+  console.log(event)
+  if (event.relatedTarget === null || event.relatedTarget.classList.contains('button')) {
+    hideAdminMenu();
+  }
+}, true));
+
+administrationList.addEventListener('blur', (event) => {
+  if (event.target.parentNode.className !== 'administration-list__item') {
+    hideAdminMenu();
+  }
+}, true);
+
+administrationBtn.addEventListener('mouseenter', showAdminMenu);
+
+document.addEventListener('keydown', (event) => {
+  console.log(event);
+  if (event.keyCode == 27) { // escape
+    hideAdminMenu();
+  }
+})
