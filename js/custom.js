37a9aabaf03368d5610db9635626d9da8edfa438
Sergey Hovakimyan <sergey_hovakimyan@epam.com>
2022-07-09 14:21:15 -0700

    fix: add arrows, enter and space handlers

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,27 @@
+function toggleTab(selectedNav, targetId) {
+  const navEls = document.querySelectorAll("#nav li a");
+  navEls.forEach(function (navEl) {
+    const id = navEl.dataset.id;
+    if (id === selectedNav) {
+      navEl.parentElement.classList.add("is-active");
+      navEl.setAttribute("aria-selected", "true");
+    } else {
+      if (navEl.parentElement.classList.contains("is-active")) {
+        navEl.parentElement.classList.remove("is-active");
+        navEl.setAttribute("aria-selected", "false");
+      }
+    }
+  });
+
+  const tabs = document.querySelectorAll(".tab-pane");
+  tabs.forEach(function (tab) {
+    tab.style.display = tab.id === targetId ? "block" : "none";
+  });
+}
+
 (function () {
-  var burger = document.querySelector(".burger");
-  var menu = document.querySelector("#" + burger.dataset.target);
+  const burger = document.querySelector(".burger");
+  const menu = document.querySelector("#" + burger.dataset.target);
   burger.addEventListener("click", function () {
     burger.classList.toggle("is-active");
     menu.classList.toggle("is-active");
@@ -12,34 +33,32 @@
     notificationCount += 1;
     region.innerText = `Notification count ${notificationCount}`;
   }, 60000);
-})();
 
-document.querySelectorAll("#nav li").forEach(function (navEl) {
-  navEl.onclick = function () {
-    toggleTab(this.id, this.dataset.target);
-  };
-});
+  document.querySelectorAll("#nav li a").forEach(function (navEl) {
+    navEl.onclick = function () {
+      toggleTab(this.dataset.id, this.dataset.target);
+    };
+  });
 
-function toggleTab(selectedNav, targetId) {
-  var navEls = document.querySelectorAll("#nav li");
-
-  navEls.forEach(function (navEl) {
-    if (navEl.id == selectedNav) {
-      navEl.classList.add("is-active");
-    } else {
-      if (navEl.classList.contains("is-active")) {
-        navEl.classList.remove("is-active");
+  const items = document.querySelectorAll("#nav li a");
+  items.forEach(function (navEl) {
+    navEl.addEventListener("keydown", function (event) {
+      const num = Number(event.target.dataset.id);
+      if (event.keyCode === 39) {
+        event.preventDefault();
+        console.log(num);
+        if (items[num]) {
+          items[num].click();
+        }
+      } else if (event.keyCode === 37) {
+        event.preventDefault();
+        if (items[num - 2]) {
+          items[num - 2].click();
+        }
+      } else if (event.keyCode === 13 || event.keyCode === 32) {
+        event.preventDefault();
+        items[num - 1].click();
       }
-    }
+    });
   });
-
-  var tabs = document.querySelectorAll(".tab-pane");
-
-  tabs.forEach(function (tab) {
-    if (tab.id == targetId) {
-      tab.style.display = "block";
-    } else {
-      tab.style.display = "none";
-    }
-  });
-}
+})();
